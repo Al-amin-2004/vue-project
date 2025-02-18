0a8e2dfc6892dec406653034be9b5d7097648718
Al amin <alaminmridha2004@gmail.com>
2025-02-18 13:28:35 +0600

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class=" h-screen bg-black">
+    <Sidebar title="My First Sidebar!"/>
+  </div>
+  
+</template>
+
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/IconDiv.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mb-3 text-center">
+        <div :class="`flex justify-center items-center rounded-lg p-2 ${!title && 'bg-white/20'}`">
+        <span v-html="icon"></span>
+    </div>
+    <p class="text-xs">{{ title }}</p>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+defineProps<{icon: string,title: string;}>();
+</script>
+
